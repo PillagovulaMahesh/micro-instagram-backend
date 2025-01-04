4d904784dede06f014836358469725607a496d4a
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-01-04 13:57:06 +0530

    Update post.model.ts

---
+++ micro-instagram-backend/src/models/post.model.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../utils/db';
+import User from './user.model';
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    images: {
+      type: DataTypes.JSON,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Post',
+    tableName: 'posts',
+    timestamps: false,
+  }
+);
+
+export default Post;
